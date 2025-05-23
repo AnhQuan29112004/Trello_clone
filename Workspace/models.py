35fcc31b3551cd12781fb7c8450ca3bc242015bd
AnhQuan29112004 <namcaodidong2004@gmail.com>
2025-05-23 18:24:16 +0700

    Feat: add some field

--- Workspace/models.py
+++ Workspace/models.py
@@ -30,24 +30,27 @@ class List(UtilModel):
         return self.name
 
 class Card(UtilModel):
-    title = models.CharField(max_length=50, null=False, blank=False)
+    name = models.CharField(max_length=50, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     file = models.FileField(upload_to='uploads/', null=True, blank=True)
     label = models.CharField(max_length=50, null=True, blank=True)
     member = models.ManyToManyField(Account, through="CardMember", related_name='membercards')
     start_date = models.DateTimeField(null=True, blank=True)
     end_date = models.DateTimeField(null=True, blank=True)
-    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name="listcard")
+    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name="listcard",null=True, blank=True)
+    tasks = models.JSONField(null=True, blank=True,default=list)
     class Meta:
-        unique_together = [['title','list']] 
+        unique_together = [['name','list']] 
     def __str__(self):
-        return self.title
+        return self.name
     
 class WorkspaceMember(models.Model):
     workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     role = models.CharField(max_length=50, choices=Account.RoleChoices.choices, default=Account.RoleChoices.WORKSPACEOWN)
-
+    class Meta:
+        unique_together = [['workspace','user']]
+    
 class CardMember(models.Model):
     card = models.ForeignKey(Card,on_delete=models.CASCADE)
     user = models.ForeignKey(Account,on_delete=models.CASCADE)
